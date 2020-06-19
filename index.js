384a0cbb1582b328c7405690e6764a2e5c05b0b5
mak7 <zacevmax@andex.ru>
2020-06-19 20:47:08 +0400

    Added correct form processing

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express')
-const { request, response } = require('express')
+const bodyParser = require('body-parser')
+// const { request, response } = require('express')
 
 const app = express()
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (request, response) => {
+  response.render('index')
+})
+
+app.post('/', (request, response) => {
+  const { city } = request.body
+
+  console.log(city)
   response.render('index')
 })
 
